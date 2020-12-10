b6ed13ed1fb0bdd36ab581a713188c903e24be71
eliasjr <a49888ce3c@emailmonkey.club>
2020-12-09 23:39:04 -0400

    map entity

--- src/main/java/com/eliasjr/brasilprev/domain/User.java
+++ src/main/java/com/eliasjr/brasilprev/domain/User.java
@@ -1,5 +1,13 @@
 package com.eliasjr.brasilprev.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import com.eliasjr.brasilprev.domain.enums.Role;
 
 import lombok.AllArgsConstructor;
@@ -11,12 +19,29 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "user")
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(length = 75, nullable = false)
 	private String name;
+
+	@Column(length = 14, nullable = false)
 	private String cpf;
+
+	@Column(length = 120, nullable = false)
 	private String address;
+
+	@Column(length = 75, nullable = false, unique = true)
 	private String email;
+
+	@Column(length = 100, nullable = false)
 	private String password;
+
+	@Column(length = 20, nullable = false, updatable = false)
+	@Enumerated(EnumType.STRING)
 	private Role role;
 }
