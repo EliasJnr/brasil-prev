0c68a867cc7ab55b14804b323819711747326103
eliasjr <a49888ce3c@emailmonkey.club>
2020-12-10 16:44:02 -0400

    create security constants

---
+++ src/main/java/com/eliasjr/brasilprev/constant/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.eliasjr.brasilprev.constant;
+
+public class SecurityConstants {
+	public static final int JWT_EXP_DAYS = 5;
+	public static final String API_KEY = "spring-brasilprev";
+	public static final String JWT_PROVIDER = "Bearer";
+	public static final String JWT_ROLE_KEY = "role";
+}
