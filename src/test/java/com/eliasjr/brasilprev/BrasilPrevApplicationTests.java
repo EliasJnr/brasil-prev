959d33a8d24ade8ab226285f28ed7a20a2db30a7
eliasjr <a49888ce3c@emailmonkey.club>
2020-12-09 23:12:36 -0400

    create project

---
+++ src/test/java/com/eliasjr/brasilprev/BrasilPrevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliasjr.brasilprev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasilPrevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
