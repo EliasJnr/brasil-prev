0793b78f60e2e5125c785f7229bd6bb9c27dd8d6
eliasjr <a49888ce3c@emailmonkey.club>
2020-12-09 23:50:55 -0400

    create user repository

---
+++ src/main/java/com/eliasjr/brasilprev/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.eliasjr.brasilprev.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.eliasjr.brasilprev.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	@Query("SELECT u FROM user u WHERE u.email = ?1 AND u.password = ?2")
+	public Optional<User> login(String email, String password);
+	
+	
+}
