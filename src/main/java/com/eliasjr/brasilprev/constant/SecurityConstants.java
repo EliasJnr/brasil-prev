aa832f554ee8a8d922f03ef535600d6f002b779b
eliasjr <a49888ce3c@emailmonkey.club>
2020-12-10 20:28:06 -0400

    add constant token invalid

--- src/main/java/com/eliasjr/brasilprev/constant/SecurityConstants.java
+++ src/main/java/com/eliasjr/brasilprev/constant/SecurityConstants.java
@@ -5,4 +5,5 @@ public class SecurityConstants {
 	public static final String API_KEY = "spring-brasilprev";
 	public static final String JWT_PROVIDER = "Bearer";
 	public static final String JWT_ROLE_KEY = "role";
+	public static final String JWT_INVALID_MSG = "Invalid Jwt token";
 }
